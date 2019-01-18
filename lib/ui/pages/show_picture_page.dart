560a2050d528ed2f42a15dc51ef0b67665be6bca
iWgang <iwgang@163.com>
2019-01-18 22:37:50 +0800

    图片增加保存本地功能

--- lib/ui/pages/show_picture_page.dart
+++ lib/ui/pages/show_picture_page.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker_saver/image_picker_saver.dart';
+import 'package:photo_view/photo_view.dart';
 
 class ShowPicturePage extends StatelessWidget {
   final String pictureUrl;
 
   ShowPicturePage(this.pictureUrl);
+
+  void _saveImage() async {
+    final res = await http.get(pictureUrl);
+    ImagePickerSaver.saveFile(fileData: res.bodyBytes).then((filePath) {
+      Fluttertoast.showToast(
+        msg: "保存成功，图片位置：$filePath",
+        toastLength: Toast.LENGTH_LONG,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +39,18 @@ class ShowPicturePage extends StatelessWidget {
                 size: 25,
               ),
               onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: EdgeInsets.only(top: 30),
+            child: IconButton(
+              icon: Icon(
+                Icons.save,
+                color: Colors.white,
+                size: 25,
+              ),
+              onPressed: () async => _saveImage(),
             ),
           ),
         ],
